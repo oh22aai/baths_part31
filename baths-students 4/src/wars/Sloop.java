0ffb645a86253c14dc87eba56d41fbf906ab6485
oh22aai <oh22aai@herts.ac.uk>
2025-04-04 00:32:22 +0100

    created sloop class

---
+++ baths-students 4/src/wars/Sloop.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wars;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Sloop extends Ship 
+{
+    private boolean doctor;
+    
+    
+    public Sloop(String nme, String captain, int cost)
+    {
+        super (nme, 5, captain,"Sloop", true, true, false, cost,0,0);
+        doctor = false;
+    }
+    
+    public void setCost()
+    {
+        super.setCost("Sloop");
+    }
+       
+    
+    public void setDoctor(String doc)
+    {
+       if (doc.equalsIgnoreCase("Yes"))
+       {
+          doctor = true;
+       } 
+       else 
+       {
+          doctor = false; 
+       }
+    }
+    
+    
+    @Override
+    public String toString()
+    {
+        String report = super.toString();
+        report += "Doctor: " + doctor + "\n";
+        return report;
+    }
+    
+}
