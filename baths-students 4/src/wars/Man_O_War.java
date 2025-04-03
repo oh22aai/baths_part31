36a3fc4ba1dc4679a8841fd7332cd7c3b5990b07
oh22aai <oh22aai@herts.ac.uk>
2025-04-04 00:31:09 +0100

    created man_o_war class

---
+++ baths-students 4/src/wars/Man_O_War.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wars;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Man_O_War extends Ship
+{
+    private int marines;
+    
+    
+    public Man_O_War(String nme, int skillLevel, String captain, int deck, int marine)
+    {
+        super( nme,skillLevel,captain,"Man-O-War",true, false,true, 0, deck,0);
+        marines = marine;
+    }
+    
+    
+    public void setCost()
+    {
+        super.setCost("Man-O-War");
+    }
+    
+    
+    
+    
+    @Override
+    public String toString()
+    {
+      String report = super.toString();
+      report += "Number of marines: " + marines + "\n";
+      return report;
+    }
+} 
