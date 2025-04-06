8d4414103c9e72b44fa8126c04fb3849c1490dfc
dw23aax-22104722 <dw23aax@herts.ac.uk>
2025-04-06 05:02:02 +0300

    Update Tester.java
    Q

--- baths-students 4/src/wars/Tester.java
+++ baths-students 4/src/wars/Tester.java
@@ -12,45 +12,44 @@ public class Tester
 {
     public static void main (String[]args)
     {
-        Man_O_War ship = new Man_O_War("Daniel",ShipState.RESERVE,3,"L",5,30);
+        Man_O_War ship = new Man_O_War("Daniel",3,"L",5,30);
         ship.setCost();
         String x = ship.toString();
-        System.out.println(ship.getShipState());
         System.out.println(x);
         
-        Man_O_War shipw = new Man_O_War("Leol",ShipState.SUNK,2,"h",2,30);
+        Man_O_War shipw = new Man_O_War("Leol",2,"h",2,30);
         shipw.setCost();
         String w = shipw.toString();
         System.out.println(w);
         
-        Sloop ships = new Sloop("Lewis",ShipState.RESTING, "fred", 500);
+        Sloop ships = new Sloop("Lewis", "fred", 500,true);
         ships.setDoctor("GOO");
         String y = ships.toString();
         System.out.println(y);
         
-        Sloop shipq = new Sloop("Lawis",ShipState.ACTIVE, "frad", 700);
+        Sloop shipq = new Sloop("Lawis", "frad", 700, false);
         shipq.setDoctor("YES");
         String p = shipq.toString();
         System.out.println(p);
         
-        Frigate shipf = new Frigate("Gerald",ShipState.RESERVE,4,"pete",60,true);
+        Frigate shipf = new Frigate("Gerald",4,"pete",60,true);
         shipf.setBlockades();
         shipf.setCost();
         String f = shipf.toString();
         System.out.println(f);
         
-        Frigate shipt = new Frigate("Gemld",ShipState.SUNK,4,"pate",40, false);
+        Frigate shipt = new Frigate("Gemld",4,"pate",40, false);
         shipt.setBlockades();
         shipt.setCost();
         String t = shipt.toString();
         System.out.println(t);
         
-        Encounter encounter_1 = new Encounter("Blockade", 5, "Riverdale", 100);
-        String s = encounter_1.toString();
+        Encounter enc20 = new Encounter("Blockade", 5, "Riverdale", 100);
+        String s = enc20.toString();
         System.out.println(s);
         
-        Encounter encounter_2 = new Encounter("Battle", 6, "Nairobi", 200);
-        String l = encounter_2.toString();
+        Encounter enc21 = new Encounter("Battle", 6, "Nairobi", 200);
+        String l = enc21.toString();
         System.out.println(l);
         
         
