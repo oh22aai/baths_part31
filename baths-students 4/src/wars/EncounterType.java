4d6bc11ac3599e6e1582074628011a64e616bd6b
dw23aax-22104722 <dw23aax@herts.ac.uk>
2025-04-07 00:28:55 +0300

    Update EncounterType.java
    LAST

--- baths-students 4/src/wars/EncounterType.java
+++ baths-students 4/src/wars/EncounterType.java
@@ -1,5 +1,12 @@
 package wars;
 
+import java.io.Serializable;
+/**
+ * Enumeration class EncounterManager - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
 import java.io.*;
 /**
  * Enumeration class EncounterType - write a description of the enum class here
